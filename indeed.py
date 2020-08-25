158cf312d7d03a3d391bb2d586ea6927dcb27b02
CHEOL HUN LEE <doulos2008@naver.cpm>
2020-08-25 14:23:57 +0200

    Extract: Indeed- title, company name

---
+++ indeed.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+URL = f"https://de.indeed.com/Jobs?q=JavaScript&l=berlin&limit={LIMIT}"
+
+
+def extract_indeed_pages():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pagination = soup.find("div", {"class": "pagination"})
+
+    links = pagination.find_all("a")
+    pages = []
+    for link in links[:-1]:
+        pages.append(int(link.string))
+
+    max_page = pages[-1]
+    return max_page
+
+
+def extract_indeed_jobs(last_page):
+    jobs = []
+    # for page in range(last_page):
+    result = requests.get(f"{URL}&start={0*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+    for result in results:
+        title = result.find("h2").find("a")["title"]
+        company = result.find("span", {"class", "company"})
+        company_anchor = company.find("a")
+        if company_anchor is not None:
+            company = company_anchor.string
+        else:
+            company = company.string
+        company = company.strip()
+        print(f"TITLE:{title}\nCOMPANY:{company}\n")
+    return jobs
